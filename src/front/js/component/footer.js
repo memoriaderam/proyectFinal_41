2110399564535f5a413bbce006899de0c936eb44
Roberto Agredo <memoriaderam@gmail.com>
2025-04-07 16:22:44 -0400

    Update footer.js
    Update footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-				
+		        <br></br>
+				<br></br>
+				<br></br>   
+				<br></br>
         <p>
 		<h6>Health/beauty 𝙊𝙥𝙩𝙞𝙘𝙖 𝙮 𝙘𝙤𝙣𝙨𝙪𝙡𝙩𝙤𝙧𝙞𝙤 𝙤𝙛𝙩𝙖𝙡𝙢𝙤𝙡𝙤𝙜𝙞𝙘𝙤 . </h6>
 		<h6>Direccion: 𝘾𝙚𝙣𝙩𝙧𝙤 𝙘𝙤𝙢𝙚𝙧𝙘𝙞𝙖𝙡 𝙇𝙤𝙧𝙙𝘾𝙚𝙣𝙩𝙚𝙧 𝙎𝙖𝙣 𝘽𝙚𝙧𝙣𝙖𝙧𝙙𝙞𝙣𝙤 Centro Comercial Lord Center, Caracas Venezuela</h6>
         </p>
 		
+		
+
 		<p>
 
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			 © 2025 Company, Inc. <i className="fa-solid fa-up-long"></i> <a href="/">· Privacy</a> <a href="/">· Terms</a>
 		</p>
+
+		
+
+
 	</footer>
 );
+
+export default Footer;
 
