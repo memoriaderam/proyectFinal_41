deb27cacd7a5fb10de8672c31b09fe40680df6cb
Iroman14 <ivanroman1498@gmail.com>
2025-04-17 15:23:44 -0600

    Roles

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
-from api.seed_data import seed_database
+from api.seed_data import seed_database, create_default_roles, create_default_user
 from flask_jwt_extended import JWTManager
 from flask import Blueprint
 from api.utils import APIException, generate_sitemap
